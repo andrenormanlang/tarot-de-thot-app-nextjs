576933f5e4159345611d61d1d71172b175affab8
andrenormanlang <andrernormanlang@gmail.com>
2024-08-31 20:28:15 +0200

    using card placeholders

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { CardModal } from "./CardModal";
-import Image from "next/image";
-import { useState } from "react";
-import { Carta, DeckProps } from "@/lib/types";
-import { Button } from "./ui/button";
-import ShufflingDeck from "@/helpers/ShufflingDeck";
-import LoadingSpinner from "@/helpers/LoadingSpinner";
-import { useFetchCard } from "@/hooks/useGetCards";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { CardModal } from './CardModal';
+import { Carta, DeckProps } from '@/lib/types';
+import ShufflingDeck from '@/helpers/ShufflingDeck';
+import LoadingSpinner from '@/helpers/LoadingSpinner';
+import { useFetchCard } from '@/hooks/useGetCards';
 
 export default function Deck({ initialCards }: DeckProps) {
   const [cards, setCards] = useState<Carta[]>(initialCards);
@@ -17,14 +17,12 @@ export default function Deck({ initialCards }: DeckProps) {
   const [modalCardId, setModalCardId] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const cardBackUrl = process.env.NEXT_PUBLIC_CARD_BACK_URL || "/CardBack.jpeg";
-  const placeholderUrl = "/question-mark.svg";
+  const cardBackUrl = process.env.NEXT_PUBLIC_CARD_BACK_URL || '/CardBack.jpeg';
+  const placeholderUrl = '/question-mark.svg';
 
   const { data: modalCard, isLoading } = useFetchCard(modalCardId);
 
-  const handleShuffle = () => {
-    setIsShuffling(true);
-  };
+  const handleShuffle = () => setIsShuffling(true);
 
   const handleSelectCard = (card: Carta) => {
     if (selectedCards.length < 3 && !selectedCards.includes(card)) {
@@ -32,9 +30,7 @@ export default function Deck({ initialCards }: DeckProps) {
     }
   };
 
-  const handleReset = () => {
-    setSelectedCards([]);
-  };
+  const handleReset = () => setSelectedCards([]);
 
   const openModal = (card: Carta) => {
     setModalCardId(card.id.toString());
@@ -50,7 +46,7 @@ export default function Deck({ initialCards }: DeckProps) {
           disabled={isShuffling || selectedCards.length > 0}
           className="bg-indigo-500 hover:bg-indigo-900 rounded-lg custom-button-1"
         >
-          {isShuffling ? "Embaralhando..." : "Embaralhar Deck"}
+          {isShuffling ? 'Embaralhando...' : 'Embaralhar Deck'}
         </Button>
       </div>
 
@@ -68,7 +64,7 @@ export default function Deck({ initialCards }: DeckProps) {
 
       {/* Selected cards */}
       <div id="selected-cards" className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-8">
-        {["Mente (Passado)", "Corpo (Presente)", "Espirito (Futuro)"].map((title, index) => (
+        {['Mente (Passado)', 'Corpo (Presente)', 'Espirito (Futuro)'].map((title, index) => (
           <div key={index} className="flex flex-col items-center">
             <h3 className="smythe-regular text-2xl font-bold mb-4 text-indigo-500">
               {title}
@@ -80,15 +76,23 @@ export default function Deck({ initialCards }: DeckProps) {
                   animate={{ opacity: 1, scale: 1 }}
                   exit={{ opacity: 0, scale: 0.8 }}
                   transition={{ duration: 0.5 }}
-                  className="card-container"
-                  onClick={() => openModal(selectedCards[index])}
+                  className={`flip-card ${selectedCards[index].isFlipped ? 'is-flipped' : ''}`}
+                  onClick={() => {
+                    const updatedCards = selectedCards.map((card) =>
+                      card.id === selectedCards[index].id
+                        ? { ...card, isFlipped: !card.isFlipped }
+                        : card
+                    );
+                    setSelectedCards(updatedCards);
+                    openModal(selectedCards[index]);
+                  }}
                 >
                   <div className="flip-card-inner">
                     <div className="flip-card-front">
                       <div className="w-full h-full relative overflow-hidden rounded-lg">
                         <Image
-                          src={selectedCards[index].url_da_imagem || "/fallback-image.jpg"}
-                          alt={selectedCards[index].nome || "Unknown Card"}
+                          src={selectedCards[index].url_da_imagem || '/fallback-image.jpg'}
+                          alt={selectedCards[index].nome || 'Unknown Card'}
                           layout="fill"
                           objectFit="cover"
                         />

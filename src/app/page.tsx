81fe45a92be8eb74611d66223b4aaf80b2c84eec
andrenormanlang <andrernormanlang@gmail.com>
2024-09-02 19:36:44 +0200

    adicionado aviso sobre largura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ export default async function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 px-4 py-8">
       <h1 className="text-4xl font-bold mb-6 text-center smythe-regular text-purple-300">
-      Tarô de Thot - Tiragem das 3 cartas     </h1>
+      Tarô de Thot - Tiragem das 3 cartas</h1>
+      <h3 className=' text-purple-300 old-standard-tt-bold'>Obs: Utilizar em telas com largura acima de 950px</h3>
       <Suspense fallback={<div>Loading...</div>}>
         <Deck initialCards={cards} />
       </Suspense>
